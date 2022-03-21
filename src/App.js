7baa57c5527e98a243ba7e875e90c851690dac84
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-21 16:14:31 -0700

    got films loading on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Films from './views/Films/Films';
+import Header from './components/Header/Header';
+
+
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className='App'> 
+        <Header />
+        <Switch> 
+          <Route exact path="/films">
+            <Films />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
+
 
 export default App;
---
+++ src/views/Main/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>Main</div>
+  );
+}
+
+// import React from 'react';
+
+
+
+// export default function Main() {
+//   const [characters, setCharcters] = useState('');
+//   const [films, setFilms] = useState ([]);
+//   const [books, setBooks] = useState ([]);
+
+//   return (
+//     <div>Main</div>
+//   )
+// }
+
